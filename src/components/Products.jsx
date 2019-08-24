a19d7a0821e15414fedf4cffa3e8fec756befb23
Ertuğrul Üngör <ertugrul.ungor@trendyol.com>
2019-08-24 09:33:40 +0300

    Update products page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,17 +7,17 @@ const Products = () => {
   return (
     <div className="d-flex justify-content-center align-items-center flex-wrap mt-5">
       {
-        data.products && data.products.map(({ imgUrl, title, description }) => {
+        data.products && data.products.map(({ id, imgUrl, title, description }) => {
           return (
             <Card style={{ width: '16rem', margin:'0 10px 10px 10px' }} key={title}>
-              <Card.Img variant="top" src={imgUrl} style={{ width: "180px", height: "auto"}} />
+              <Card.Img variant="top" src={imgUrl} style={{ width: "180px", height: "auto", marginLeft: "auto", marginRight: "auto"}} />
               <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>
                   {description}
                 </Card.Text>
                 <Link to={`product/${title}`}>
-                  <Button variant="primary">Ürüne Git</Button>
+                  <Button variant="primary" disabled={id !== 1}>Ürüne Git</Button>
                 </Link>
               </Card.Body>
             </Card>
